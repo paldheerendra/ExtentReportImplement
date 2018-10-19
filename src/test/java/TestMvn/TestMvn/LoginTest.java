49807711167d1003e93f71b0d42f2113c3a5c7cc
Dheerendra Pal <Dheerendra.Pal@sqs.com>
2018-10-19 11:30:08 +0530

    Listeners entry added to testng.xml

---
+++ src/test/java/TestMvn/TestMvn/LoginTest.java
@@ -0,0 +1,78 @@
+package TestMvn.TestMvn;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import TestMvn.Base;
+
+public class LoginTest extends Base {
+		
+	WebDriver driver;
+	
+	@BeforeMethod()
+	public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\6867\\Downloads\\Browsers\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		//driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get("https://www.sqs.com/en/index.php");
+	}
+	
+	@Test(priority=0)
+	public void test() {
+		System.out.println("*************TestNg is perfectly running*********");
+	}
+	
+	@Test(priority=1)
+	public void aboutusPageTest() {
+		
+		WebElement aboutlement=driver.findElement(By.xpath("//a[text()='About Us']"));
+		aboutlement.click();
+		
+		
+	}
+	
+	@Test(priority=3)
+	public void technologiesPageTest() {
+			
+		driver.findElement(By.xpath("//a[text()='Technologies']")).click();;
+	}
+	
+	
+	@Test(priority=4)
+	public void sapPageTest() {
+			
+		WebElement element=driver.findElement(By.xpath("//a[text()='Technologies']"));
+		
+		Actions action=new Actions(driver);
+		action.moveToElement(element).build().perform();
+		
+		WebElement ele=driver.findElement(By.xpath("//a[text()='SAP Expertise']"));
+		
+		explicitWait(driver, ele, 30);
+		ele.click();
+		
+				
+	}
+	
+
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.close();
+	}
+	
+
+}
